3935dced2afed82068ebf076601f1428d2b7d643
Elvis <levgcsc@gmail.com>
2017-08-25 10:45:03 +0800

    Feat/add module of SwaggerUI

---
+++ swagger-ui/src/main/java/com/lesson/learn/swaggerui/SwaggerUiApplication.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017. @author lev-gc
+ */
+
+package com.lesson.learn.swaggerui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Swagger ui application.
+ */
+@SpringBootApplication
+public class SwaggerUiApplication {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+		SpringApplication.run(SwaggerUiApplication.class, args);
+	}
+
+}
