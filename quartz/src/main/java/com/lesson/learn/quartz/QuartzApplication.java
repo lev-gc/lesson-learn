a557f5008d1a01433aaf160b6d12edd9917c9ef8
Elvis <levgcsc@gmail.com>
2017-09-04 15:20:40 +0800

    Feat/add package of quartz

---
+++ quartz/src/main/java/com/lesson/learn/quartz/QuartzApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2017. @author lev-gc
+ */
+
+package com.lesson.learn.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzApplication.class, args);
+	}
+}
