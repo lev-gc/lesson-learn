2c63588c6c75f9a47471408f32faa51f411b03ed
Elvis <levgcsc@gmail.com>
2017-09-22 17:07:40 +0800

    Feat/add module of JavaMail

---
+++ java-mail/src/main/java/com/lesson/learn/javamail/JavaMailApplication.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2017. @author lev-gc
+ */
+
+package com.lesson.learn.javamail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <b><code>Application</code></b>
+ * <p/>
+ * Application of Java Mail
+ * <p/>
+ * <b>Creation Time:</b> 2017/9/21 9:42.
+ *
+ * @author Elvis
+ * @since java-mail 0.0.1
+ */
+@SpringBootApplication
+public class JavaMailApplication {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMailApplication.class, args);
+    }
+
+}
