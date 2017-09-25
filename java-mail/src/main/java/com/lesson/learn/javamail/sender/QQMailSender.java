311453b7578d6ee40b10e062bcb46200804a4ab8
Elvis <levgcsc@gmail.com>
2017-09-25 19:07:01 +0800

    Feat/add QQMail sender

--- java-mail/src/main/java/com/lesson/learn/javamail/sender/QQMailSender.java
+++ java-mail/src/main/java/com/lesson/learn/javamail/sender/QQMailSender.java
@@ -12,16 +12,16 @@ import java.util.Date;
 import java.util.Properties;
 
 /**
- * <b><code>GmailSender</code></b>
+ * <b><code>QQMailSender</code></b>
  * <p/>
- * GmailSender
+ * the QQMail[exmail] sender
  * <p/>
  * <b>Creation Time:</b> 2017/9/21 10:20.
  *
  * @author Elvis
  * @since java-mail 0.0.1
  */
-public class GmailSender {
+public class QQMailSender {
 
     /**
      * The Template of Gmail Sender.
@@ -34,26 +34,21 @@ public class GmailSender {
 
         Properties props = new Properties();
 //        props.setProperty("mail.debug", "true");
+        props.setProperty("mail.transport.protocol", "smtp");
         props.setProperty("mail.smtp.auth", "true");
-        props.setProperty("mail.smtp.host", "smtp.gmail.com");
-        // use Type SSL
-        props.setProperty("mail.smtp.ssl.enable", "true");
-        props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        props.setProperty("mail.smtp.port", "465");
-        props.setProperty("mail.smtp.socketFactory.port", "465");
-        // or use Type TLS
-//        props.put("mail.smtp.starttls.enable", "true");
-//        props.put("mail.smtp.port", "587");
+        props.setProperty("mail.host", "smtp.qq.com"); // or "smtp.exmail.qq.com"
 
         Session session = Session.getInstance(props, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("sender@gmail.com", "********");
+                // NOTE: the password here is not the password of your QQMail[or exmail], but the hash sign provided by Tencent.
+                return new PasswordAuthentication("sender@**.com", "********");
             }
         });
 
+        // Message Object
         Message message = new MimeMessage(session);
         // From
-        message.setFrom(new InternetAddress("sender@gmail.com", "Alias of sender"));
+        message.setFrom(new InternetAddress("sender@**.com", "Alias of sender"));
         // TO
         message.setRecipients(Message.RecipientType.TO, new InternetAddress[]{
                 new InternetAddress("ToReceiver1@*****.com", "Alias of TO receiver1"),
@@ -71,7 +66,7 @@ public class GmailSender {
         // set Title
         message.setSubject("JavaMail Test");
         // set Content
-        message.setText("Here is the mail send by JavaMail using Gmail!");
+        message.setText("Here is the mail send by JavaMail using QQMail[or exmail]!");
         // set Send Date
         message.setSentDate(new Date());
 
