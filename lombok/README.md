7563c6a1bbbdc1132aa70fc9e4820cd6c820d6c3
Elvis <levgcsc@gmail.com>
2018-07-05 12:16:53 +0800

    Feat/add module of lombok

---
+++ lombok/README.md
@@ -0,0 +1,10 @@
+#  Lombok
+
+> The usage example of Lombok.
+
+How to start:
+1. Startup by LogbackApplication.class
+2. open the url http://localhost:8180/logback/
+
+
+see : http://www.w2bc.com/article/94182
---
+++ lombok/src/main/java/com/lesson/learn/lombok/LombokApplication.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2018. @author lev-gc
+ */
+
+package com.lesson.learn.lombok;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <b><code>LombokApplication</code></b>
+ * <p/>
+ * Application of Lombok
+ * <p/>
+ * <b>Creation Time:</b> 2018/7/4 12:14.
+ *
+ * @author Elvis
+ * @since lombok 0.1.0
+ */
+@SpringBootApplication
+public class LombokApplication {
+
+    /**
+     * The constant LOG.
+     */
+    private final static Logger LOG = LoggerFactory.getLogger(LombokApplication.class);
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        LOG.info("Application Start!");
+        SpringApplication.run(LombokApplication.class, args);
+    }
+
+}
